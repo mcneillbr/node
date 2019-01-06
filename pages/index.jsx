5bb2e2b220d3689795032f68aad3cd68a3d19566
Marcelo <mcneill.marcelo@gmail.com>
2019-01-06 14:12:21 -0200

    test next.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,20 @@
-export default () => <div>Welcome to next.js!</div>
+import "../styles.scss"
+import Head from 'next/head'
+
+export default () => {
+    const imgStyle = {width: "128px", height: "128px"};
+    const divStyle = {
+        color: 'white',
+        backgroundColor: "#f4f4f4",
+        WebkitTransition: 'all', // note the capital 'W' here
+        msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+      };
+    return ( <div className="example">
+    <Head>
+      <title>example next.js</title>
+      </Head>
+        <img src="static/images/loginr_key.png" alt=""  style={{width: "128px", height: "128px"}} />
+    <p style={divStyle}>Welcome to next.js!</p>
+    </div>
+    );
+}
